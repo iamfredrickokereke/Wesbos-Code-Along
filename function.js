5a57ccc6cd8944b9865fce4e73839a6fa3668330
Kelly online <kellyonline2016@gmail.com>
2020-05-25 20:20:34 +0100

    calling and returning functions

---
+++ function.js
@@ -0,0 +1,16 @@
+function calculateBill() {
+    let total = 10 * 1.13;
+    console.log('Calculate is running');
+    return total;
+}
+
+const myTotal = calculateBill();
+
+console.log(myTotal);
+
+
+// console.log(total);// scope issues
+
+
+
+
