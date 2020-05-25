5813f5b8a0bfe13d05a419f74bf4e5ac071d3b59
Kelly online <kellyonline2016@gmail.com>
2020-05-25 22:29:04 +0100

    calling and returning functions

--- function.js
+++ function.js
@@ -1,19 +1,42 @@
 
-// Function definition
-function calculateBill(billAmount, taxRate) {
-    let total = billAmount * 1 + taxRate;
-    console.log('Calculate is running');
-    return total;
+// // Function definition
+// function calculateBill(billAmount, taxRate) {
+//     let total = billAmount * (1 + taxRate);
+//     console.log('Calculate is running');
+//     return total;
+// }
+
+
+// const fredBill = 1200;
+// const fredTax = 0.2;
+
+
+// const test = calculateBill(fredBill, fredTax);
+
+// console.log(test, 'test...');
+
+// // function call and storing in a variable
+// const myTotal = calculateBill(30, 1.4);
+// console.log(myTotal);
+
+
+
+
+// // console.log(total);// scope issues
+
+
+
+
+
+function sayHi(params) {
+    return `Hey ${params.toUpperCase()}`;
 }
 
-// function call and storing in a variable
-const myTotal = calculateBill();
 
-console.log(myTotal);
+function yell(params) {
+    return `Dr. ${params}`;
+}
 
+const test = yell('fred');
 
-// console.log(total);// scope issues
-
-
-
-
+console.log(test);
