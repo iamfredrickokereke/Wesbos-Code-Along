8ffd8851e4adf0071977fe73218a1736f2e93cf6
Kelly online <kellyonline2016@gmail.com>
2020-05-26 16:49:00 +0100

    modifiedfile

--- inches2cm.js
+++ inches2cm.js
@@ -8,16 +8,3 @@
 //     return  params_inches * 2.54;
 // }
 // console.log(inchToCM(34));
-  
-function makeBaby(firstname, lastname) {
-    const baby = {
-        name : `${firstname} ${lastname}`,
-        age : 0
-    }
-
-    console.log(baby);
-    return baby;
-}
-
-
-makeBaby('fred', 'okereke')
