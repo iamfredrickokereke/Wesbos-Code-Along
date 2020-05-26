db342729b3124a3520495bad7cf4e38812511603
Kelly online <kellyonline2016@gmail.com>
2020-05-26 15:28:45 +0100

    added hoisting file

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+
+console.log(`My name is {firstname('fred')} and my last name is {lastName('okereke')}`);
+
+
+
+function firstname(params) {
+    return params;
+}
+
+const lastname = function lastName(params) {
+    return params;
+}
